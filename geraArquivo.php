a634f8e2b9b49a8998258552746ef643a2580478
Fabricio Matrix <86535948+fabriciomatrix@users.noreply.github.com>
2021-06-26 17:53:38 -0300

    Add files via upload

---
+++ geraArquivo.php
@@ -0,0 +1,90 @@
+<?php 
+
+$bl_01_01 = $_POST['bl_01_01'];
+$bl_01_02 = $_POST['bl_01_02'];
+$bl_01_03 = $_POST['bl_01_03'];
+
+$bl_02_01 = $_POST['bl_02_01'];
+$bl_02_02 = $_POST['bl_02_02'];
+$bl_02_03 = $_POST['bl_02_03'];
+
+$bl_03_01 = $_POST['bl_03_01'];
+$bl_03_02 = $_POST['bl_03_02'];
+$bl_03_03 = $_POST['bl_03_03'];
+
+$xxx1 = $_POST['xxx1'];
+$xxx2 = $_POST['xxx2'];
+$xxx3 = $_POST['xxx3'];
+
+$bl_ipv6 = $_POST['bl_ipv6'];
+
+$dominio = $_POST['dominio'];
+
+$ns13 = $_POST['ns1-3'];
+$ns14 = $_POST['ns1-4'];
+
+//Pega os dados do arquivo
+$dataFile = file_get_contents("named.conf.local_default.txt","r");
+
+//Altera os dados do arquivo (agora com as novas variáveis)
+
+$dataFile = str_replace("{{bl_01_01}}",$bl_01_01, $dataFile);
+$dataFile = str_replace("{{bl_01_02}}",$bl_01_02, $dataFile);
+$dataFile = str_replace("{{bl_01_03}}",$bl_01_03, $dataFile);
+
+$dataFile = str_replace("{{bl_02_01}}",$bl_02_01, $dataFile);
+$dataFile = str_replace("{{bl_02_02}}",$bl_02_02, $dataFile);
+$dataFile = str_replace("{{bl_02_03}}",$bl_02_03, $dataFile);
+
+$dataFile = str_replace("{{bl_03_01}}",$bl_03_01, $dataFile);
+$dataFile = str_replace("{{bl_03_02}}",$bl_03_02, $dataFile);
+$dataFile = str_replace("{{bl_03_03}}",$bl_03_03, $dataFile);
+
+$dataFile = str_replace("{{xxx1}}",$xxx1, $dataFile);
+$dataFile = str_replace("{{xxx2}}",$xxx2, $dataFile);
+$dataFile = str_replace("{{xxx3}}",$xxx3, $dataFile);
+
+$dataFile = str_replace("{{bl_ipv6}}",$bl_ipv6, $dataFile);
+
+$dataFile = str_replace("{{dominio}}",$dominio, $dataFile);
+//Cria um novo arquivo
+$novoArquivo = fopen("named.conf.local", "w+");
+
+/////////////////////////////////////////////////////////////////////////////////
+//Pega os dados do arquivo
+$dataFile2 = file_get_contents("dominio.net.br.zone_default.txt","r");
+
+$dataFile2 = str_replace("{{bl_ipv6}}",$bl_ipv6, $dataFile2);
+
+$dataFile2 = str_replace("{{bl_01_01}}",$bl_01_01, $dataFile2);
+$dataFile2 = str_replace("{{bl_01_02}}",$bl_01_02, $dataFile2);
+$dataFile2 = str_replace("{{ns13}}",$ns13, $dataFile2);
+$dataFile2 = str_replace("{{ns14}}",$ns14, $dataFile2);
+
+$dataFile2 = str_replace("{{dominio}}",$dominio, $dataFile2);
+
+
+//Cria um novo arquivo
+$novoArquivo2 = fopen(''.$dominio.'.zone', 'w+');
+
+///////////////////////////////////////////////////////////////////////////////
+
+if($novoArquivo){
+    //Se o arquivo realmente foi criado, é escrito o novo script nele
+    if(fwrite($novoArquivo, $dataFile)){
+        print_r("Arquivo criado com sucesso");
+    }else{
+        print_r("Não consegui criar o arquivo");
+    }
+}
+
+if($novoArquivo2){
+    //Se o arquivo realmente foi criado, é escrito o novo script nele
+    if(fwrite($novoArquivo2, $dataFile2)){
+        print_r("Arquivo2 criado com sucesso");
+    }else{
+        print_r("Não consegui criar o arquivo2");
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv=”Content-Type” content=”text/html; charset=iso-8859-1″>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://www.terabitconsultoria.com/imagens/icon-terabit.png" />
+</head>
+<body>
+    <center><a href="https://www.terabitconsultoria.com/" target="_blank"><img src="https://www.terabitconsultoria.com/imagens/logo-terabit.png" alt="Terabit Consultoria"></a>
+    <center><h2>Bind Script Generator v1.0</h2>        
+    <title>BSG v1.0</title>    
+    <link rel="stylesheet" href="style.css" type="text/css" media="all" />
+    <div class="content">
+        <form action="geraArquivo.php" method="POST">
+
+             <div class="dominioVariavel">
+                <label> Domínio </label>                
+                <input type="text" placeholder='  sem "www"' name="dominio" size="42">               
+            </div><br/>
+			
+            <div class="ns1Variavel">
+                <label> NS1 </label>
+				<label>xxx.xxx.</label>
+                <input type="text" name="ns1-3" size="5">
+                <input type="text" name="ns1-4" size="5">
+                <label>.</label>				
+
+            </div><br/>			
+			
+
+            <div class="primeiraVariavel">
+                <label> Bloco /24 - 01 </label>
+                <input type="text" name="bl_01_01" size="5">          
+                <label> . </label>
+                <input type="text" name="bl_01_02" size="5">             
+                <label> . </label>
+                <input type="text" name="bl_01_03" size="5">             
+                <label> .0/24 </label>
+            </div><br/>
+
+            <div class="segundaVariavel">
+                <label> Bloco /24 - 02 </label>
+                <input type="text" name="bl_02_01" size="5">          
+                <label> . </label>
+                <input type="text" name="bl_02_02" size="5">             
+                <label> . </label>
+                <input type="text" name="bl_02_03" size="5">             
+                <label> .0/24 </label>
+            </div><br/>
+
+            <div class="terceiraVariavel">                
+                <label> Bloco /24 - 03 </label>
+                <input type="text" name="bl_03_01" size="5">          
+                <label> . </label>
+                <input type="text" name="bl_03_02" size="5">             
+                <label> . </label>
+                <input type="text" name="bl_03_03" size="5">             
+                <label> .0/24 </label>
+            </div><br/>
+
+            <div class="quartaVariavel">
+                <label> Bloco /24 - 04 </label>
+                <input type="text" name="xxx1" size="5">          
+                <label> . </label>
+                <input type="text" name="xxx2" size="5">             
+                <label> . </label>
+                <input type="text" name="xxx3" size="5">             
+                <label> .0/24 </label>
+            </div><br/>
+
+            <div class="quintaVariavel">
+                <label> Bloco IPv6/32 </label>
+                <label><b>2804: </label></b>
+                <input type="text" name="bl_ipv6" size="5">                 
+                <label> ::/32 </label>
+            </div><br/>
+
+            <div class="dnsVariavel">
+                <label> Registro </label>
+                <input type="text" size="5">          
+                <label> Tipo </label>
+                     <select id="dns">
+                        <option name="A" value="tipoA">A</option>
+                        <option name="CNAME" value="cname">CNAME</option>
+                        <option name="TXT" value="txt">TXT</option>
+                        <option name="MX" value="mx">MX</option>
+                      </select>                            
+                <label> Dados </label>
+                <input type="text" size="10">             
+                <label>  </label>
+            </div><br/>
+
+            <div class="dnsVariavel">
+                <label> Registro </label>
+                <input type="text" size="5">          
+                <label> Tipo </label>
+                     <select id="dns">
+                        <option name="A" value="tipoA">A</option>
+                        <option name="CNAME" value="cname">CNAME</option>
+                        <option name="TXT" value="txt">TXT</option>
+                        <option name="MX" value="mx">MX</option>
+                      </select>                            
+                <label> Dados </label>
+                <input type="text" size="10">             
+                <label>  </label>
+            </div><br/>
+
+            <div class="dnsVariavel">
+                <label> Registro </label>
+                <input type="text" size="5">          
+                <label> Tipo </label>
+                     <select id="dns">
+                        <option name="A" value="tipoA">A</option>
+                        <option name="CNAME" value="cname">CNAME</option>
+                        <option name="TXT" value="txt">TXT</option>
+                        <option name="MX" value="mx">MX</option>
+                      </select>                            
+                <label> Dados </label>
+                <input type="text" size="10">             
+                <label>  </label>
+            </div><br/>
+            <button type="submit"> Enviar </button>
+        </form> 
+    </div>
+</body>
+</html>
